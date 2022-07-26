bf23b2f24dd4d803cf8c87a76e88017f80154cc8
Iván <cortes.ivan353@gmail.com>
2022-07-26 10:10:59 -0600

    heroku update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import App from './components/App'
 import './styles/variables.scss'
 import './styles/index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <App />
-  </>
+  <BrowserRouter>
+    <Routes>
+      <Route path={'/'} exact element={<App/>}/>
+    </Routes>
+  </BrowserRouter>
 )
